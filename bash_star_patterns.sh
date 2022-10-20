0ef8b2d749150aab439205ecccb73f185b7cd844
nimrod <nbesman@gmail.com>
2022-10-20 22:47:59 +0300

    first bash commit

---
+++ bash_star_patterns.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+#! /bin/bash
+SIDE_ARROW():{
+num=5
+row=1
+col=1
+colum=1
+while [[ $row -le $num ]] 
+do
+col=1
+     while [[ $col -le $num*2 ]]
+     do
+	if [[ $col -ge $row*2-1 && $col -le $row+4 ]]
+	then
+	  echo -n "*"
+	else
+	  echo -n " "
+	fi
+	((col++))
+	done
+echo ""
+((row++)) 
+done
+num=5	
+row=1
+col=num*2-1
+while [[ $row -le 4 ]]	
+do
+col=num*2-1
+	while [[ $col -ge 2 ]]
+	do
+	if [[ $col -le $row*2+1 && $col-$row -ge 1 ]] 
+	then
+	echo -n "*"
+	else
+	echo -n " "
+	fi
+	((col--))
+	done	
+echo " "
+((row++))
+done
+}
+
+SIDE_ARROW
