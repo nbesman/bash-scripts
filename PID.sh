98263d2e003baddb351c2b3c3058d010e3fcb417
nimrod <nbesman@gmail.com>
2022-11-04 17:51:01 +0200

    second commit PID.dh

--- PID.sh
+++ PID.sh
@@ -9,7 +9,7 @@ function PROC()
     procc=$(ps -ax | awk '{print $1}' | grep ^$pid$)
      #echo procc = $procc
     if [[ $procc != $pid ]]; then
-    	echo pid $pid is down > /home/nbesman/warning-pid:$pid:is-down.txt
+    	echo pid $pid is down > tee /home/nbesman/warning-pid:$pid:is-down.txt
     else
     	echo pid $pid is up 
     fi
